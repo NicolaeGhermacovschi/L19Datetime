673cd45f6a7b43b81de54fbd886b116011974440
Nicolae Ghermacovschi <ghermacovschi.nicolae@gmail.com>
2023-03-16 17:15:15 +0200

    second

---
+++ src/Date.java
@@ -0,0 +1,41 @@
+import java.time.*;
+
+public class Date {
+    public Date() {
+    }
+    public LocalDate method1(){
+        LocalDate dateTime = LocalDate.of(2024, Month.JANUARY,2);
+        return dateTime;
+    }
+
+    public LocalTime method2(){
+        LocalTime timeHours = LocalTime.of(13,20);
+        return timeHours;
+    }
+    public LocalDateTime method3(){
+       LocalDateTime ld = LocalDateTime.of(method1(), method2());
+        return ld;
+    }
+    public ZonedDateTime method4(){
+        ZoneId zone = ZoneId.of("US/Eastern");
+        ZonedDateTime zd = ZonedDateTime.of(method3(), zone );
+        return zd;
+    }
+    public LocalDateTime method5(){
+        LocalDate date = method1();
+        LocalTime time = method2();
+        time = time.plusMinutes(1);
+        time = time.plusHours(1);
+        date = date.plusDays(1);
+        date = date.plusYears(1);
+
+        LocalDateTime dateTime1 = LocalDateTime.of(date, time);
+
+
+        return dateTime1;
+
+    }
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.time.ZoneId;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Date date = new Date();
+        System.out.println(date.method1());
+        System.out.println(date.method2());
+        System.out.println(date.method3());
+        System.out.println(date.method4());
+        System.out.println(date.method5());
+
+    }
+}
